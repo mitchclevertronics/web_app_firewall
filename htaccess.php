ace83f69865ebf1c3a6386b197a7c761493efd97
Roman Shneer <roman@barriliance.com>
2016-11-08 14:41:19 +0200

    stop injection if no mod_rewrite

--- htaccess.php
+++ htaccess.php
@@ -4,6 +4,7 @@
  * License: GNU
  * Copyright 2016 WebAppFirewall RomanShneer <romanshneer@gmail.com>
  */
+
 session_start();
 require_once "libs/db.inc.php";
 require_once "libs/waf_report.class.php";
@@ -21,6 +22,11 @@ if(isset($_POST['op'])&&isset($_POST['content']))
  fclose($f);
 }
 
+$opts=array('file_e'=>file_exists($filename)?true:false,
+				   'file_w'=>is_writable($filename)?true:false,
+				   'mod_rewrite'=>in_array('mod_rewrite', apache_get_modules())
+				   );
+
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
           "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"  xml:lang="en" lang="en">
@@ -31,10 +37,23 @@ if(isset($_POST['op'])&&isset($_POST['content']))
 <?php include_once 'include/header.php';?>   
 		<h1 class='title'>Edit .htaccess for redirect code injection</h1>		
 		<div class='box'>
-				<strong><?php echo $filename;?></strong><br>
+				<h3 style="text-align:center"><?php echo $filename;?></h3>
+				<table style="margin:5px auto;">
 					
-						File exists: <?php echo file_exists($filename)?'Yes':'No';?><br>		
-						File writeble: <?php echo is_writable($filename)?'Yes':'No';?><br>	
+					<tr>
+						<td>File exists:</td>
+						<td><?php echo ($opts['file_e'])?'<font style="color:green;">Yes</font>':'<font style="color:red;font-weight:bold;">No</font>';?></td>
+					</tr>
+					<tr>
+						<td>File writeble:</td>
+						<td><?php echo ($opts['file_w'])?'<font style="color:green;">Yes</font>':'<font style="color:red;font-weight:bold;">No</font>';?></td>
+					</tr>
+					<tr>
+						<td>Mod_Rewrite enabled:</td>
+						<td><?php echo ($opts['mod_rewrite'])?'<font style="color:green;">Yes</font>':'<font style="color:red;font-weight:bold;">No</font>';?></td>
+					</tr>
+				</table>
+				<?php if(($opts['file_e'])&&($opts['file_w'])&&($opts['mod_rewrite'])):?>
 						<div class='description'>	
 								<ol>
 										<li>Backup origin .htaccess file</li>
@@ -55,6 +74,9 @@ RewriteRule ^(.*)$ <?php echo $folder;?>/waf.php [N,L]</textarea></div>
 									<textarea name='content' rows='40' class="inset textarea"><?php echo file_exists($filename)?file_get_contents($filename):"";?></textarea>
 						<input type="submit" name="op" value="Save" class="green_btn">
 								</form>		
+				<?php else:?>
+					<center style="color:red">Impossible inject to .htaccess code, because one of the reasons above.</center>
+				<?php endif;?>				
 		</div>
 </body>
 </html>		
